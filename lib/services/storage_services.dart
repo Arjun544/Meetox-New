026ab74e7e9d9a0f435f3af20337fad0eeba404f
arjun544 <arjunsingh.mahar55@gmail.com>
2023-07-30 17:17:53 +0500

    Fixed get public url

--- lib/services/storage_services.dart
+++ lib/services/storage_services.dart
@@ -1,23 +1,19 @@
 import 'dart:io';
 
 import '../core/imports/core_imports.dart';
-import '../core/imports/packages_imports.dart';
 
 class StorageServices {
   static Future<String> uploadImage(
       RxBool isLoading, String folder, File file) async {
     try {
       isLoading(true);
-      final path = await supabase.storage.from(folder).upload(
+      await supabase.storage.from(folder).upload(
             '${supabase.auth.currentUser!.id}/profile',
             file,
           );
 
       final String publicUrl = supabase.storage.from(folder).getPublicUrl(
-            path,
-            transform: const TransformOptions(
-              quality: 50,
-            ),
+            '/${supabase.auth.currentUser!.id}/profile',
           );
       isLoading(false);
       return publicUrl;
